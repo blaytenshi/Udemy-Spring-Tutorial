e230fc598f56e4051d84d489cf3a215dcf6b0d6d
Jimmy Tang <jimmyguojing.tang@uts.edu.au>
2016-02-12 17:41:38 +1100

    Udemy Spring Tutorial 11 - Bean Scope

---
+++ src/main/java/com/caveofprogramming/spring/test/App.java
@@ -0,0 +1,22 @@
+package com.caveofprogramming.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		
+		// Creates Spring container that will go fetch beans from the name of the xml file specified
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/beans/beans.xml");
+		
+		// passing bean id into the getBean method and cast the return type of the object
+		Person person1 = (Person)context.getBean("person");
+		Person person2 = (Person)context.getBean("person");
+		person1.setTaxId(666);
+
+		System.out.println(person2);
+		
+		((ClassPathXmlApplicationContext) context).close();
+	}
+}

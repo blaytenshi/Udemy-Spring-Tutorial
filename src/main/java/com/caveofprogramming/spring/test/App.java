838cfcfb174b5e80534a1e4a9ba3c1625b6fb49d
Jimmy Tang <blay.tenshi@gmail.com>
2016-02-13 23:24:09 +1100

    Udemy Spring Tutorial 14 - Setting List Values

--- src/main/java/com/caveofprogramming/spring/test/App.java
+++ src/main/java/com/caveofprogramming/spring/test/App.java
@@ -2,24 +2,16 @@ package com.caveofprogramming.spring.test;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
 		
-		// Creates Spring container that will go fetch beans from the name of the xml file specified
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/beans/beans.xml");
 		
-		// passing bean id into the getBean method and cast the return type of the object
-		Person person = (Person)context.getBean("person");
+		FruitBasket basket = (FruitBasket)context.getBean("basket");
 		
-		person.setTaxId(666);
-
-		System.out.println(person);
+		System.out.println(basket);
 		
-		Address address2 = (Address)context.getBean("address2");
-		System.out.println(address2);
-		
-		((ClassPathXmlApplicationContext) context).close();
+		((ClassPathXmlApplicationContext)context).close();
 	}
 }

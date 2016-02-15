91e77c512bdfc09c7973a28a501425a14afe64c2
Jimmy Tang <jimmyguojing.tang@uts.edu.au>
2016-02-15 14:37:19 +1100

    Udemy Spring Tutorial 21 - Autowire by Constructor

--- src/main/java/com/caveofprogramming/spring/test/Logger.java
+++ src/main/java/com/caveofprogramming/spring/test/Logger.java
@@ -4,6 +4,25 @@ public class Logger {
 	private LogWriter consoleWriter;
 	private LogWriter fileWriter;
 	
+	
+	public Logger(ConsoleWriter consoleWriter, FileWriter fileWriter) {
+		this.consoleWriter = consoleWriter;
+		this.fileWriter = fileWriter;
+	}
+	
+	/*
+	 * These will only enable autowire to wire one of them leaving the other 
+	 * unwired and the method call in App will fail with null pointer exception.
+	 * 
+	public Logger(ConsoleWriter consoleWriter) {
+		this.consoleWriter = consoleWriter;
+	}
+	
+	public Logger(FileWriter fileWriter) {
+		this.fileWriter = fileWriter;
+	}
+	*/
+	
 	public void setConsoleWriter(LogWriter writer) {
 		this.consoleWriter = writer;
 	}

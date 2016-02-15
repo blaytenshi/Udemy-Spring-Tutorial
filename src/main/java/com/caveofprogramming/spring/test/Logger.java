c8cbb510ffd0778d946de158193948198661abe8
Jimmy Tang <jimmyguojing.tang@uts.edu.au>
2016-02-15 16:06:12 +1100

    Udemy Spring Tutorial 25 - The Autowire Annotation

--- src/main/java/com/caveofprogramming/spring/test/Logger.java
+++ src/main/java/com/caveofprogramming/spring/test/Logger.java
@@ -1,14 +1,31 @@
 package com.caveofprogramming.spring.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Logger {
 	
+	/*
+	 * Autowired only works by type!
+	 */
+	
+	@Autowired
 	private ConsoleWriter consoleWriter;
+	@Autowired
 	private FileWriter fileWriter;
 	
+	/*
+	@Autowired
+	public Logger(ConsoleWriter consoleWriter, FileWriter fileWriter) {
+		this.consoleWriter = consoleWriter;
+		this.fileWriter = fileWriter;
+	}
+	*/
+
 	public void setConsoleWriter(ConsoleWriter writer) {
 		this.consoleWriter = writer;
 	}
 	
+	// @Autowired -- You don't even need these set methods!
 	public void setFileWriter(FileWriter fileWriter) {
 		this.fileWriter = fileWriter;
 	}

c7db1a18a932c2437a4eaca80042178f4a3d1ca9
Jimmy Tang <jimmyguojing.tang@uts.edu.au>
2016-02-15 17:25:04 +1100

    Udemy Spring Tutorial 27 - Qualifier Annotation

--- src/main/java/com/caveofprogramming/spring/test/Logger.java
+++ src/main/java/com/caveofprogramming/spring/test/Logger.java
@@ -1,22 +1,33 @@
 package com.caveofprogramming.spring.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Logger {
 
 	private ConsoleWriter consoleWriter;
-	private FileWriter fileWriter;
+	private LogWriter fileWriter;
 
-	@Autowired(required = false)
+	@Autowired
+	@Qualifier("toconsole")
 	/*
-	 * Even though the Autowired annotation is declared here, even if it is not declared in the bean.xml, Spring will not crash.
+	 * Because in beans.xml there is more than one bean that implements ConsoleWriter you'll get a
+	 * "expected single matching bean but found 2: consoleWriter,squirrel" error
+	 * But by utilising the Qualifier Annotation, we tell beans.xml which method to wire into the context
+	 * when the writeConsole method is called by App
 	 */
 	public void setConsoleWriter(ConsoleWriter writer) {
 		this.consoleWriter = writer;
 	}
 
 	@Autowired
-	public void setFileWriter(FileWriter fileWriter) {
+	@Qualifier("filewriter")
+	/*
+	 * This is declaring Qualifiers by annotating the Class as opposed to the beans.xml. By annotating the
+	 * class, this tells Spring which class to wire in when there is an ambiguity in which class to use when
+	 * Spring encounters an interface being wired into the bean and being called by App.
+	 */
+	public void setFileWriter(LogWriter fileWriter) {
 		this.fileWriter = fileWriter;
 	}
 

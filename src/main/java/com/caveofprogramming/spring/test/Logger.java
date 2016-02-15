eeefdcd6348eabed6ea1cf4d81dc61bc97524572
Jimmy Tang <jimmyguojing.tang@uts.edu.au>
2016-02-15 17:35:00 +1100

    Udemy Spring Tutorial 29 - JSR 250 Post Init Pre DestroY

--- src/main/java/com/caveofprogramming/spring/test/Logger.java
+++ src/main/java/com/caveofprogramming/spring/test/Logger.java
@@ -1,6 +1,6 @@
 package com.caveofprogramming.spring.test;
 
-import javax.annotation.Resource;
+import javax.annotation.*;
 
 public class Logger {
 
@@ -25,6 +25,23 @@ public class Logger {
 		if (consoleWriter != null) {
 			consoleWriter.write(text);
 		}
+	}
+	
+	/*
+	 * Run after bean is created. Name of method doesn't matter.
+	 */
+	@PostConstruct
+	public void init() {
+		System.out.println("init");
+	}
+	
+	/*
+	 * Run before bean is destroyed. Again name of method doesn't matter.
+	 * NOTE: where you declare the bean in beans.xml they must be a singleton or default otherwise Spring won't destroy it.
+	 */
+	@PreDestroy
+	public void destroy() {
+		System.out.println("destroy");
 	}
 
 }

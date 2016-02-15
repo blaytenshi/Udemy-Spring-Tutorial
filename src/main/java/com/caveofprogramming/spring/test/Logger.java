90e431e79e5b89aa74e58cf9b73724d138019026
Jimmy Tang <jimmyguojing.tang@uts.edu.au>
2016-02-15 17:30:21 +1100

    Udemy Spring Tutorial 28 - JSR 250

--- src/main/java/com/caveofprogramming/spring/test/Logger.java
+++ src/main/java/com/caveofprogramming/spring/test/Logger.java
@@ -1,32 +1,18 @@
 package com.caveofprogramming.spring.test;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import javax.annotation.Resource;
 
 public class Logger {
 
 	private ConsoleWriter consoleWriter;
 	private LogWriter fileWriter;
 
-	@Autowired
-	@Qualifier("toconsole")
-	/*
-	 * Because in beans.xml there is more than one bean that implements ConsoleWriter you'll get a
-	 * "expected single matching bean but found 2: consoleWriter,squirrel" error
-	 * But by utilising the Qualifier Annotation, we tell beans.xml which method to wire into the context
-	 * when the writeConsole method is called by App
-	 */
+	@Resource
 	public void setConsoleWriter(ConsoleWriter writer) {
 		this.consoleWriter = writer;
 	}
-
-	@Autowired
-	@Qualifier("filewriter")
-	/*
-	 * This is declaring Qualifiers by annotating the Class as opposed to the beans.xml. By annotating the
-	 * class, this tells Spring which class to wire in when there is an ambiguity in which class to use when
-	 * Spring encounters an interface being wired into the bean and being called by App.
-	 */
+	
+	@Resource(name="squirrel")
 	public void setFileWriter(LogWriter fileWriter) {
 		this.fileWriter = fileWriter;
 	}

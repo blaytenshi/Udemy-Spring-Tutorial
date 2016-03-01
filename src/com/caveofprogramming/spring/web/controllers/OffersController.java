d93130e025cd8ce4c3f992fec5aae946e2b266f5
Jimmy Tang <blay.tenshi@gmail.com>
2016-03-01 22:25:35 +1100

    Udemy Spring Tutorial 69 - Adding Hibernate Validation

--- src/com/caveofprogramming/spring/web/controllers/OffersController.java
+++ src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -2,9 +2,13 @@ package com.caveofprogramming.spring.web.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,9 +52,20 @@ public class OffersController {
 	}
 	
 	@RequestMapping(value="/doCreate", method=RequestMethod.POST)
-	public String doCreate(Model model, Offer offer) {
-		// It'll automatically map the input into the object!! :O!!!
-		System.out.println(offer);
+	public String doCreate(Model model, @Valid Offer offer, BindingResult result) {
+		// It'll automatically map the input into the Offer object!! :O!!!
+		
+		if(result.hasErrors()) {
+			System.out.println("Form does not validate.");
+			List<ObjectError> errors = result.getAllErrors();
+			
+			for(ObjectError error : errors) {
+				System.out.println(error.getDefaultMessage());
+			}
+		} else {
+			System.out.println("Form validated");
+		}
+		
 		return "offerCreated";
 	}
 	

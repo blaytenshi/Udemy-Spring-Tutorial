741920fc2a756f809981d5ddd4bec7b533049733
Jimmy Tang <jimmyguojing.tang@uts.edu.au>
2016-03-01 14:48:11 +1100

    Udemy Spring Tutorial 64 - Getting URL Parameters

--- src/com/caveofprogramming/spring/web/controllers/OffersController.java
+++ src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.caveofprogramming.spring.web.dao.Offer;
 import com.caveofprogramming.spring.web.service.OfferService;
@@ -20,6 +22,14 @@ public class OffersController {
 		this.offerService = offerService;
 	}
 	
+	// If you only supply the value this is what it looks like
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	public String showTest(Model model, @RequestParam("id") String id) {
+		
+		System.out.println("Id is: " + id);
+		
+		return "home";
+	}
 	
 	@RequestMapping("/offers")
 	public String showOffers(Model model) {
